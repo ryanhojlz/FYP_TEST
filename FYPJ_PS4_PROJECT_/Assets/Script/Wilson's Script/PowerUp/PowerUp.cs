12cede415a99006ea14667008bb58c1ca81150ff
Totally Not Going To Tell <neoh.wilson@gmail.com>
2019-03-22 18:12:02 +0800

    merge

---
+++ FYPJ_PS4_PROJECT_/Assets/Script/Wilson's Script/PowerUp/PowerUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    protected Minion minion_unit;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        minion_unit = gameObject.GetComponent<Minion>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public virtual void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
